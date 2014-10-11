47c31b0be876a9d6e13a361d250cd147af616c5f
Jiří Kareš <kupker@gmail.com>
2014-10-11 17:41:53 +0200

    Entity: Hotel, Room, Booking - one-to-many bidirectional relations

--- trunk/BookingManager/src/main/java/bookingmanager/entity/Booking.java
+++ trunk/BookingManager/src/main/java/bookingmanager/entity/Booking.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 /**
  *
@@ -20,6 +21,9 @@ public class Booking implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    
+    @ManyToOne
+    private Room room;
 
     public Long getId() {
         return id;
