3f50bb4adb499cdfa71851e66b78d00aa2b5f0ea
Jakub Sestak <sestak.jakub@gmail.com>
2014-11-05 18:33:25 +0100

    added other DTO

--- BookingManager-API/src/main/java/cz/muni/fi/pa165/bookingmanager/api/dto/CustomerDTO.java
+++ BookingManager-API/src/main/java/cz/muni/fi/pa165/bookingmanager/api/dto/CustomerDTO.java
@@ -5,10 +5,81 @@
  */
 package cz.muni.fi.pa165.bookingmanager.api.dto;
 
+import java.util.List;
+
 /**
  *
  * @author Jakub
  */
 public class CustomerDTO {
+    
+    public CustomerDTO() {}
+    
+    private long id;
+    
+    private String name;
+    
+    private String address;
+    
+    private List<BookingDTO> bookings;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<BookingDTO> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<BookingDTO> bookings) {
+        this.bookings = bookings;
+    }
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + (int) (this.id ^ (this.id >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CustomerDTO other = (CustomerDTO) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer[ id=" + id + ", name= " + name + " ,address: " + address + "]";
+    }
     
 }
--- BookingManager-API/src/main/java/cz/muni/fi/pa165/bookingmanager/api/dto/RoomDTO.java
+++ BookingManager-API/src/main/java/cz/muni/fi/pa165/bookingmanager/api/dto/RoomDTO.java
@@ -5,10 +5,100 @@
  */
 package cz.muni.fi.pa165.bookingmanager.api.dto;
 
+import java.util.List;
+
 /**
  *
  * @author Jakub
  */
 public class RoomDTO {
+    private long id;
+
+    private int roomNumber;
+
+    private HotelDTO hotel;
+
+    private int capacity;
+
+    private float price;
     
+    private List<BookingDTO> bookings;
+
+    public RoomDTO() {
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setRoomNumber(int n) {
+        roomNumber = n;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setHotel(HotelDTO hotel) {
+        this.hotel = hotel;
+    }
+
+    public HotelDTO getHotel() {
+        return hotel;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public List<BookingDTO> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<BookingDTO> bookings) {
+        this.bookings = bookings;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 83 * hash + (int) (this.id ^ (this.id >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final RoomDTO other = (RoomDTO) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+    
+    @Override
+    public String toString() {
+        return "Room[ id=" + id + ", room number=" + roomNumber + ", capacity: " + capacity + " ]";
+    }
 }
