3c7e2efbd94297d61f0711fd3c8693794511ee7d
Jiří Kareš <kupker@gmail.com>
2014-11-07 21:44:35 +0100

    HotelServiceImpl some exceptions

--- BookingManager-API/src/main/java/cz/muni/fi/pa165/bookingmanager/api/service/impl/HotelServiceImpl.java
+++ BookingManager-API/src/main/java/cz/muni/fi/pa165/bookingmanager/api/service/impl/HotelServiceImpl.java
@@ -21,41 +21,36 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class HotelServiceImpl implements HotelService {
-    
+
     private HotelDTOConverter hotelConverter = new HotelDTOConverter();
-    
     @Autowired
     private HotelDAO hotelDAO;
 
     @Override
     public void addHotel(HotelDTO hotelDTO) {
-        
-        if (hotelDTO == null){
+
+        if (hotelDTO == null) {
             throw new IllegalArgumentException("Hotel is null");
-        }
-        else if (hotelDTO.getId() < 0)
-            throw new IllegalArgumentException("Hotel id is negative");
-        else if (hotelDTO.getAddress() == "")
+        } else if (hotelDTO.getAddress() == "") {
             throw new IllegalArgumentException("Hotel address is empty");
-        else if (hotelDTO.getName() == "")
+        } else if (hotelDTO.getName() == "") {
             throw new IllegalArgumentException("Hotel name is empty");
-        else if (hotelDTO.getPhoneNumber() == "")
+        } else if (hotelDTO.getPhoneNumber() == "") {
             throw new IllegalArgumentException("Hotel phone number is empty");
-        
+        }
+
         hotelDAO.persistHotel(hotelConverter.dtoToEntity(hotelDTO));
     }
 
     @Override
     public void deleteHotel(HotelDTO hotelDTO) {
-        
-        if (hotelDTO == null){
+
+        if (hotelDTO == null) {
             throw new IllegalArgumentException("Hotel is null");
         }
-        
+
         hotelDAO.removeHotel(hotelConverter.dtoToEntity(hotelDTO));
     }
-    
-    
 
     @Override
     public List<HotelDTO> getAllHotels() {
@@ -66,9 +61,14 @@ public class HotelServiceImpl implements HotelService {
     public void updateHotel(HotelDTO hotelDTO) {
         if (hotelDTO == null) {
             throw new IllegalArgumentException("null parameter");
+        } else if (hotelDTO.getAddress() == "") {
+            throw new IllegalArgumentException("Hotel address is empty");
+        } else if (hotelDTO.getName() == "") {
+            throw new IllegalArgumentException("Hotel name is empty");
+        } else if (hotelDTO.getPhoneNumber() == "") {
+            throw new IllegalArgumentException("Hotel phone number is empty");
         }
-        
+
         hotelDAO.mergeHotel(hotelConverter.dtoToEntity(hotelDTO));
     }
-    
 }
