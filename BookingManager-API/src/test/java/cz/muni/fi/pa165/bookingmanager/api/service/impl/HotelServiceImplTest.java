aec3718dc1367f52f22a869beb9cd6f76dbb4fb6
japarmezan <jani.michalkova@gmail.com>
2014-11-07 15:39:36 +0100

    Mock Tests: first try for HotelServiceImplTest

---
+++ BookingManager-API/src/test/java/cz/muni/fi/pa165/bookingmanager/api/service/impl/HotelServiceImplTest.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.bookingmanager.api.service.impl;
+
+import cz.muni.fi.pa165.bookingmanager.api.converter.HotelDTOConverter;
+import cz.muni.fi.pa165.bookingmanager.api.dto.HotelDTO;
+import cz.muni.fi.pa165.bookingmanager.api.dto.RoomDTO;
+import cz.muni.fi.pa165.bookingmanager.api.service.HotelService;
+import cz.muni.fi.pa165.bookingmanager.backend.db.HotelDAO;
+import cz.muni.fi.pa165.bookingmanager.backend.db.impl.HotelDAOImpl;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+import static org.mockito.Mockito.*;
+
+
+/**
+ *
+ * @author Jana
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class HotelServiceImplTest {
+    
+    public HotelServiceImplTest() {
+    }
+    
+    @InjectMocks
+    private HotelServiceImpl service;
+    
+    @Mock
+    private HotelDAOImpl hotelDAO;
+    
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addHotel method, of class HotelServiceImpl.
+     */
+    @Test
+    public void testAddHotel() {
+        System.out.println("addHotel");
+        
+        HotelDTO hotelDTO = newHotelDTO();
+        
+        service.addHotel(hotelDTO);
+        
+        verify(hotelDAO, Mockito.times(1)).persistHotel(HotelDTOConverter.dtoToEntity(hotelDTO));
+        
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddHotelAsNull() {
+        System.out.println("addHotelAsNull");
+        
+        HotelDTO hotelDTO = null;
+        
+        service.addHotel(hotelDTO);
+        
+        verifyZeroInteractions(hotelDAO);
+    }
+
+    /**
+     * Test of deleteHotel method, of class HotelServiceImpl.
+     */
+    @Test
+    public void testDeleteHotel() {
+        System.out.println("deleteHotel");
+        
+        HotelDTO hotelDTO = newHotelDTO();
+        
+        service.addHotel(hotelDTO);
+        service.deleteHotel(hotelDTO);
+        
+        verify(hotelDAO, Mockito.times(1)).removeHotel(HotelDTOConverter.dtoToEntity(hotelDTO));
+    }
+
+    /**
+     * Test of getAllHotels method, of class HotelServiceImpl.
+     */
+    @Test
+    public void testGetAllHotels() {
+        System.out.println("getAllHotels");
+        
+        HotelDTO hotelDTO = newHotelDTO();
+        
+        service.addHotel(hotelDTO);
+        service.getAllHotels();
+        
+        verify(hotelDAO, Mockito.times(1)).getAllHotels();
+    }
+    
+    private HotelDTO newHotelDTO(){
+        HotelDTO hotel = new HotelDTO();
+        hotel.setAddress("Botanicka 14");
+        hotel.setName("Modra ustrica");
+        hotel.setPhoneNumber(123456);
+        
+        List<RoomDTO> rooms = Arrays.asList(new RoomDTO());
+        hotel.setRooms(rooms);
+        
+        return hotel;
+    }
+    
+}
